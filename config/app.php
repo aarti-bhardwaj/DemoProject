b254ffcec075ad680d88949769b4c2603400e696
kshitizsekhri <kshitiz.sekhri@twinspark.co>
2016-09-02 14:15:22 +0530

    bootstrap

--- config/app.php
+++ config/app.php
@@ -230,8 +230,8 @@ return [
              */
             //'port' => 'non_standard_port_number',
             'username' => 'root',
-            'password' => 'root',
-            'database' => 'facebook',
+            'password' => 'saritaanil',
+            'database' => 'DemoProject',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
